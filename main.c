81b02d512cad2e882b01174e85f84696c68ded66
jiwoo01 <gwangbok1004@sookmyung.ac.kr>
2020-11-19 16:38:00 +0900

    practice 7

--- main.c
+++ main.c
@@ -7,19 +7,31 @@ int main(int argc, char *argv[]) {
 	
 	FILE *fp;
 	char str[100];
-
+	char path[100];//파일경로
+	char lookup[100];//찾고자 하는 단어
 	
-	fp = fopen("sample.txt", "r");
+	printf("input file path:");
+	scanf("%s", path);
+	printf("input search word:");
+	scanf("%s", lookup); 
+	
+	fp = fopen( path, "r");
 	if(fp == NULL)
 	{
+		printf("invaild path! (%s)\n", path);
 		return -1;
 	}
 	 
 	while( fgets(str, 100, fp) != NULL)  
 	{
-	 	printf("%s", str);
+		if(strncmp(str, lookup, strlen(lookup) ) == 0)
+		{
+			printf("Search succeed!\n");
+			fclose(fp);
+			return 0;
+		}
 	}
+	 printf("Search failed!\n");
 	 fclose(fp);
-	 
-	return 0;
+	 return 0;
 }
